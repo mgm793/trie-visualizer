8fcca9ce7e0cf5f4d4832b7272f7bed5e88713aa
Marc <mgm793@gmail.com>
2022-07-14 15:39:03 +0200

    refactor: adding addNode function to isolate draw functionality

--- src/Visualizer.js
+++ src/Visualizer.js
@@ -22,21 +22,27 @@ export class Visualizer {
         const y = (((this.circleRadius * 2) + this.spaceBetweenCircles) * i) + this.circleRadius + this.canvasPadding;
         return [x,y];
     }
+
+    #addNode(x,y,value,isFinishedWord){
+        this.ctx.beginPath();
+        this.ctx.arc(x,y, this.circleRadius, 0, 2 * Math.PI);
+        this.ctx.stroke();
+        this.ctx.fillStyle = isFinishedWord ? 'lightgreen' : 'lightgrey';
+        this.ctx.fill();
+        this.ctx.beginPath();
+        this.ctx.fillStyle = 'black';
+        this.ctx.font = 'bold 15px Arial';
+        this.ctx.fillText(value.toUpperCase(), x, y, 45);
+        this.ctx.fill;
+    }
+
     render(){
         const trieMatrix = this.trie.getTrieAsMatrix();
         for(let i = 0; i < trieMatrix.length; ++i){
             for(let j = 0; j < trieMatrix[i].length; ++j){
                 const [x,y] = this.#getCoord(i,j);
-                this.ctx.beginPath();
-                this.ctx.arc(x,y, this.circleRadius, 0, 2 * Math.PI);
-                this.ctx.stroke();
-                this.ctx.fillStyle = trieMatrix[i][j].isFinishedWord ? 'lightgreen' : 'lightgrey';
-                this.ctx.fill();
-                this.ctx.beginPath();
-                this.ctx.fillStyle = 'black';
-                this.ctx.font = 'bold 15px Arial';
-                this.ctx.fillText(trieMatrix[i][j].value.toUpperCase(), x, y, 45);
-                this.ctx.fill;
+                const { value, isFinishedWord} = trieMatrix[i][j];
+                this.#addNode(x,y, value, isFinishedWord);
             }
         }
     }
